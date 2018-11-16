5d0e6effa676a3128d6c92213534325476107674
Michael Egner <michael@theventuresmiths.com>
2018-11-16 10:16:19 +0200

    Basic Docs page created

--- src/docs/Docs.js
+++ src/docs/Docs.js
@@ -3,6 +3,32 @@ import Navigation from './Navigation';
 import ComponentPage from './ComponentPage';
 import componentData from '../../config/componentData';
 
+//no router used, just keeping URL in state
+
 export default class Docs extends React.Component {
-    // more to follow...
+    constructor (props) {
+        super(props);
+        this.state = {
+            route: window.location.hash.substr(1)
+        }
+    }
+
+    //updates the route when the fragment (#) portion of the URL changes
+    componentDidMount() {
+        window.addEventListener('hashchange', () => {
+            this.setState({route: window.location.hash.substr(1)})
+        })
+    }
+
+    render() {
+        const {route} = this.state;
+        //look for component within componentData (a list) based on the current route 
+        const component = route ? componentData.filter(component => component.name === route)[0] : componentData[0];
+        return (
+            <div>
+                <Navigation components={componentData.map(component => component.Name)} />
+                <ComponentPage component={component} />
+            </div>
+        )
+    }
 };

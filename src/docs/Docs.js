4ae28e123c04313af5a744823fbcc55dd74fb3c0
Michael Egner <michael@theventuresmiths.com>
2018-11-16 09:22:25 +0200

    setup basic Docs file

---
+++ src/docs/Docs.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Navigation from './Navigation';
+import ComponentPage from './ComponentPage';
+import componentData from '../../config/componentData';
+
+export default class Docs extends React.Component {
+    // more to follow...
+};
